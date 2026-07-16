f7af1bca96dce0cd21bf2a3b59875cee5aa0a749
Roberto Paulo Souza Chagas de Lima <roberto123chagas@gmail.com>
2026-07-15 20:48:54 -0400

    Initial commit

---
+++ src/content/site.ts
@@ -0,0 +1,24 @@
+export const site = {
+  name: "ACSC Advogadas",
+  slogan: "Defendendo seus direitos com ética e excelência.",
+  subtitulo: "Atendimento jurídico personalizado para pessoas e empresas, oferecendo segurança, transparência e compromisso em cada caso.",
+  phone: "",
+  whatsapp: "(67) 99887-7665",
+  email: "contato@acscadv.com.br/",
+
+  address: "",
+  botaoPrimario:"Agendar Consulta",
+  botaoSecundario:"Fale no WhatsApp",
+
+  menu: {
+    sobre: "Sobre",
+    areas: "Áreas de Atuação",
+    contato: "Contato",
+  },
+
+  social: {
+    instagram: "",
+    facebook: "",
+    linkedin: "",
+  },
+};
